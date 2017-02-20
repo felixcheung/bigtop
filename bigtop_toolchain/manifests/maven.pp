056ed3a5c0ae35ad3a24d20559e2e883c495be8b
Felix Cheung <felixcheung@users.noreply.github.com>
2017-02-19 18:24:46 -0800

    revert maven change

--- bigtop_toolchain/manifests/maven.pp
+++ bigtop_toolchain/manifests/maven.pp
@@ -29,10 +29,10 @@ class bigtop_toolchain::maven {
     creates     => "/usr/local/$mvn",
     require     => Exec["/usr/bin/wget $apache_prefix/maven/maven-3/$mvnversion/binaries/$mvn-bin.tar.gz"],
   }
-
-  file {'/usr/local/mvn':
+ 
+  file {'/usr/local/maven':
     ensure  => link,
-    target  => "/usr/local/$mvn/bin/mvn",
+    target  => "/usr/local/$mvn",
     require => Exec["/bin/tar xvzf /usr/src/$mvn-bin.tar.gz"],
   }
 }
