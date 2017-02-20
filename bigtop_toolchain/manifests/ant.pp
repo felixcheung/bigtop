db170c3272b149bb78a8e967bd9e68b68a8b4bf2
Felix Cheung <felixcheung@users.noreply.github.com>
2017-02-19 16:39:05 -0800

    update ant link

--- bigtop_toolchain/manifests/ant.pp
+++ bigtop_toolchain/manifests/ant.pp
@@ -31,7 +31,7 @@ class bigtop_toolchain::ant {
 
   file {'/usr/local/ant':
     ensure  => link,
-    target  => "/usr/local/$ant",
+    target  => "/usr/local/$ant/bin/ant",
     require => Exec["/bin/tar xvzf /usr/src/$ant-bin.tar.gz"],
   }
 }
